f254851cc2c7f008aaa4a91d1616113e894a2c7b
Francesco <frybene@libero.it>
2025-10-03 21:38:20 +0200

    .

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def load_from_pickle(name):
     with open(name, 'rb') as f:
         return pickle.load(f)
 
+
 def create_dataframes(df, afl, model_name, dim, dst_dir):
     for k in list(afl.keys()):
         if k.__contains__("duplicate"):
@@ -22,18 +23,20 @@ def create_dataframes(df, afl, model_name, dim, dst_dir):
     center = [int(k) for k in list(afl.keys()) if afl[k] == "center"]
     right = [int(k) for k in list(afl.keys()) if afl[k] == "right"]
     far_right = [int(k) for k in list(afl.keys()) if afl[k] == "far right"]
-    unknown = [int(k) for k in list(afl.keys()) if afl[k] == "unknown"]
+    unknown = None
+    if "unknown" in list(afl.keys()):
+        unknown = [int(k) for k in list(afl.keys()) if afl[k] == "unknown"]
     apolitical = [int(k) for k in list(afl.keys()) if afl[k] == "non political"]
 
-    df = df.drop(columns=[k for k in df.columns if k not in ["account_id", "content"]])
+    df = df.drop(columns=[k for k in df.columns if k not in ["account_id", "posts_count", "content"]])
     df_far_left = df[df.account_id.isin(far_left)]
     df_left = df[df.account_id.isin(left)]
     df_center = df[df.account_id.isin(center)]
     df_right = df[df.account_id.isin(right)]
     df_far_right = df[df.account_id.isin(far_right)]
-    df_unknown = df[df.account_id.isin(unknown)]
+    if unknown:
+        df_unknown = df[df.account_id.isin(unknown)]
     df_apolitical = df[df.account_id.isin(apolitical)]
-
 
     with pd.ExcelWriter(f"{dst_dir}/{model_name}_{dim}.xlsx") as writer:
         df_far_left.to_excel(writer, sheet_name="far_left")
@@ -41,7 +44,8 @@ def create_dataframes(df, afl, model_name, dim, dst_dir):
         df_center.to_excel(writer, sheet_name="center")
         df_right.to_excel(writer, sheet_name="right")
         df_far_right.to_excel(writer, sheet_name="far_right")
-        df_unknown.to_excel(writer, sheet_name="can't decide")
+        if unknown:
+            df_unknown.to_excel(writer, sheet_name="can't decide")
         df_apolitical.to_excel(writer, sheet_name="apolitical")
 
         for sheet in writer.sheets:
@@ -50,4 +54,3 @@ def create_dataframes(df, afl, model_name, dim, dst_dir):
             for row in worksheet.iter_rows():
                 for cell in row:
                     cell.font = Font(size=14)
-
