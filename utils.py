863ed1d7c0d7246a52c739d30489c008427424ea
Frank <frybene@libero.it>
2025-10-03 16:38:39 +0200

    modified function for excel creation

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pickle
 
 import pandas as pd
+from openpyxl.styles import Font
 
 
 def save_to_pickle(name, c):
@@ -12,30 +13,41 @@ def load_from_pickle(name):
     with open(name, 'rb') as f:
         return pickle.load(f)
 
-def create_dataframes(df, afl, model_name, dim):
+def create_dataframes(df, afl, model_name, dim, dst_dir):
     for k in list(afl.keys()):
         if k.__contains__("duplicate"):
             afl.pop(k)
-    far_right = [int(k) for k in list(afl.keys()) if afl[k] == "far right"]
     far_left = [int(k) for k in list(afl.keys()) if afl[k] == "far left"]
     left = [int(k) for k in list(afl.keys()) if afl[k] == "left"]
-    right = [int(k) for k in list(afl.keys()) if afl[k] == "right"]
-    apolitical = [int(k) for k in list(afl.keys()) if afl[k] == "non political"]
     center = [int(k) for k in list(afl.keys()) if afl[k] == "center"]
+    right = [int(k) for k in list(afl.keys()) if afl[k] == "right"]
+    far_right = [int(k) for k in list(afl.keys()) if afl[k] == "far right"]
+    unknown = [int(k) for k in list(afl.keys()) if afl[k] == "unknown"]
+    apolitical = [int(k) for k in list(afl.keys()) if afl[k] == "non political"]
 
     df = df.drop(columns=[k for k in df.columns if k not in ["account_id", "content"]])
+    df_far_left = df[df.account_id.isin(far_left)]
     df_left = df[df.account_id.isin(left)]
+    df_center = df[df.account_id.isin(center)]
     df_right = df[df.account_id.isin(right)]
     df_far_right = df[df.account_id.isin(far_right)]
-    df_far_left = df[df.account_id.isin(far_left)]
-    df_center = df[df.account_id.isin(center)]
+    df_unknown = df[df.account_id.isin(unknown)]
     df_apolitical = df[df.account_id.isin(apolitical)]
 
 
-    with pd.ExcelWriter(f"stance/{model_name}_{dim}.xlsx") as writer:
-        df_right.to_excel(writer, sheet_name="right")
-        df_left.to_excel(writer, sheet_name="left")
+    with pd.ExcelWriter(f"{dst_dir}/{model_name}_{dim}.xlsx") as writer:
         df_far_left.to_excel(writer, sheet_name="far_left")
-        df_far_right.to_excel(writer, sheet_name="far_right")
+        df_left.to_excel(writer, sheet_name="left")
         df_center.to_excel(writer, sheet_name="center")
+        df_right.to_excel(writer, sheet_name="right")
+        df_far_right.to_excel(writer, sheet_name="far_right")
+        df_unknown.to_excel(writer, sheet_name="can't decide")
         df_apolitical.to_excel(writer, sheet_name="apolitical")
+
+        for sheet in writer.sheets:
+            worksheet = writer.sheets[sheet]
+            # Set font size for all cells
+            for row in worksheet.iter_rows():
+                for cell in row:
+                    cell.font = Font(size=14)
+
