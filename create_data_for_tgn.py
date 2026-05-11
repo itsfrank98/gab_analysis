83b246aee8b9f1626f79045137f1ad10c0ce5bdc
itsfrank98 <fmiolanista@gmail.com>
2026-05-11 18:12:51 +0200

    update

--- create_data_for_tgn.py
+++ create_data_for_tgn.py
@@ -1,6 +1,11 @@
 """
 In questo file creiamo i csv necessari per lanciare tgn. Al momento le interazioni gestite sono follow, authorship, comment, user evolution. Se vuoi considerare un altro
 tipo di interazione devi prima creare i file negli snapshot, e poi implementare qui la gestione di quell'interazione
+
+IMPORTANTE:
+type_interaction values:
+- 0 indica il follow
+- 1 indica l'evoluzione degli utenti
 """
 
 from utils import load_from_pickle, save_to_pickle
@@ -121,24 +126,27 @@ def interaction_follow_dataset_creation(src, features, full_users_set, mapping, 
 
 if __name__ == "__main__":
     CREATE_TENSOR = False
-    CONSIDER_SYNTHETIC = False
+    CONSIDER_SYNTHETIC = True
     CREATE_INTERACTION_DF = True
     RESET_NETWORK = True
     SHUFFLE_UNTIL = None  # -3 Se settato a none non fa lo shuffling temporale (snapshot ordinati in maniera casuale)
 
     # BERT FEATURES PATHS
-    BERT_FEATURES_REAL_POSTS_SRC = "features_bert/bert_features_real_posts.pkl"
-    BERT_FEATURES_REAL_COMMENTS_SRC = "features_bert/bert_features_real_comments.pkl"
-    BERT_FEATURES_SYNTHETIC_SRC = None  # "features_bert/bert_features_synthetic.pkl"
+    BERT_FEATURES_REAL_POSTS_SRC = "dataset/features_bert/bert_features_real_posts.pkl"
+    BERT_FEATURES_REAL_COMMENTS_SRC = "dataset/features_bert/bert_features_real_comments.pkl"
+    BERT_FEATURES_SYNTHETIC_SRC = "dataset/features_bert/bert_features_few_shot_enriched.pkl"
+
 
     df_names = ["comments_current_snapshot.csv", "posts_current_snapshot.csv"]
     processed_contents_src = ["comments_only_posting_users.csv", "posts_processed.csv"]
 
-    df_name = df_names[1]  #  Se settato a posts_incremental fa la divisione normale del dataset. Settalo a posts_current_snapshot per il caso baseline (nessun ordinamento temporale, solo train/val/test)
+    df_name = df_names[1]
     BASE = "dataset"
     SNAPSHOT_SRC = "files_for_tgn/snapshots"        # "baseline"
     CONTENT_PROCESSED_SRC = os.path.join(BASE, processed_contents_src[1])
     OUTPUT_GAB_DF_NAME = "gab_posts"
+    CREATE_SYNTHETIC_SNAPSHOTS = 5
+    synthetic_posts_src = os.path.join(BASE, "synthetic_posts_irix_fewshot_enriched.csv")
 
     MAP_SRC = "files_for_tgn/mapping.pkl"    # "mapping_baseline.pkl"
     INV_MAP_SRC ="files_for_tgn/inv_mapping.pkl"  # "inv_mapping_baseline.pkl"
@@ -149,11 +157,21 @@ if __name__ == "__main__":
     inv_mapping = load_from_pickle(os.path.join(BASE, INV_MAP_SRC))
     mapping = load_from_pickle(os.path.join(BASE, MAP_SRC))
 
+    if CREATE_SYNTHETIC_SNAPSHOTS > 0:
+        synthetic_posts = pd.read_csv(synthetic_posts_src)
+        shuffled = synthetic_posts.sample(frac=1)
+        chunk_size = len(synthetic_posts)/CREATE_SYNTHETIC_SNAPSHOTS
+        dfs = [shuffled.iloc[i * chunk_size:(i + 1) * chunk_size] for i in range(CREATE_SYNTHETIC_SNAPSHOTS)]
+        for i, df in enumerate(dfs):
+            df.to_csv(os.path.join(SNAPSHOT_SRC, f"batch{i}_synthetic.csv"), index=False)
+
+    #TODO CREA GLI SNAPSHOT E POI LANCIA IL RESTO DEL CODICE
+
+    # branch that manages the interactions in the real dataset
     if CREATE_INTERACTION_DF:
         full_df = pd.read_csv(CONTENT_PROCESSED_SRC)
         users = set(full_df["account_id"].tolist())
-        #df = interaction_follow_dataset_creation(src=os.path.join(BASE, SNAPSHOT_SRC), features=features, full_users_set=users,
-        #                                         mapping=mapping, df_name=df_name, shuffle_until=SHUFFLE_UNTIL, reset_network=RESET_NETWORK)
+
         interaction_type = 0
         if df_name.__contains__("comment"):
             interaction_type = 2
@@ -162,8 +180,16 @@ if __name__ == "__main__":
             interaction_type = 3
             features = load_from_pickle(os.path.join(BASE, BERT_FEATURES_REAL_POSTS_SRC))
 
-        df = interaction_authorship_dataset_creation(src=os.path.join(BASE, SNAPSHOT_SRC), content_features=features, df_name=df_name, type_interaction=interaction_type)
+        # gestisce il follow e l'evoluzione degli utenti da uno snapshot all'altro
+        df = interaction_follow_dataset_creation(src=os.path.join(BASE, SNAPSHOT_SRC), features=features,
+                                                 full_users_set=users,
+                                                 mapping=mapping, df_name=df_name, shuffle_until=SHUFFLE_UNTIL,
+                                                 reset_network=RESET_NETWORK)
+
+        # TODO la riga commentata sotto è per l'estensione, in cui consideriamo i commenti e le relazioni utente --writes--> post
+        #df = interaction_authorship_dataset_creation(src=os.path.join(BASE, SNAPSHOT_SRC), content_features=features, df_name=df_name, type_interaction=interaction_type)
         df.to_csv(os.path.join(BASE, SNAPSHOT_SRC, OUTPUT_GAB_DF_NAME + ".csv"))
+    # branch that manages the synthetic dataset
     if CONSIDER_SYNTHETIC:
         df = pd.read_csv(os.path.join(BASE, SNAPSHOT_SRC, OUTPUT_GAB_DF_NAME + ".csv"))
         df = df.drop(columns=[c for c in df.columns if c.__contains__("Unnamed")])
@@ -172,7 +198,8 @@ if __name__ == "__main__":
         node_feat_dim = synthetic_features[list(synthetic_features.keys())[0]].shape[0]
         last_ts = sorted(df["timestamp"].tolist())[-1]+1
         synthetic_df_list = [os.path.join(BASE, f"batch{i}_synthetic.csv") for i in [1, 2, 3]]
-        synthetic_df_list.append(os.path.join(BASE, "totti_vangogh.csv"))
+        already_initialized_users = []
+        #synthetic_df_list.append(os.path.join(BASE, "totti_vangogh.csv"))
         for d in synthetic_df_list:
             synthetic_df = pd.read_csv(d)
             users = synthetic_df["account_id"].drop_duplicates().tolist()
@@ -180,15 +207,18 @@ if __name__ == "__main__":
                 posts_by_user = synthetic_df[synthetic_df["account_id"] == u]["id"].tolist()
                 posts_features = [synthetic_features[i] for i in posts_by_user]
                 avg = torch.stack(posts_features).mean(dim=0).numpy()
-                ld.append({"follower_id": mapping[u], "followed_id": mapping[u], "timestamp": last_ts, "state_label": 0,
+                ld.append({"follower_id": mapping[u], "followed_id": mapping[u], "timestamp": last_ts, "state_label": 0,        # Evoluzione dell'utente nello snapshot corrente
                            "comma_separated_list_of_features": ",".join(str(e) for e in avg), "type_interaction": 1})
-                ld.append({"follower_id": mapping[u], "followed_id": mapping[u], "timestamp": 0, "state_label": 0,
-                           "comma_separated_list_of_features": ",".join(str(e) for e in np.zeros(node_feat_dim)), "type_interaction": 1})
+                if u not in already_initialized_users:
+                    ld.append({"follower_id": mapping[u], "followed_id": mapping[u], "timestamp": 0, "state_label": 0,              # Aggiungo l'utente con features = 0 nello snapshot 0
+                               "comma_separated_list_of_features": ",".join(str(e) for e in np.zeros(node_feat_dim)), "type_interaction": 1})
+                    already_initialized_users.append(u)
             last_ts += 1
 
         df_synth = pd.DataFrame(ld)
         final_df = pd.concat([df, df_synth]).sort_values(by=["timestamp"])
         final_df.to_csv(os.path.join(BASE, OUTPUT_GAB_DF_NAME + "_with_synthetic.csv"))
+
     if CREATE_TENSOR:
         dfs_l = []
         for d in os.listdir(BASE):
